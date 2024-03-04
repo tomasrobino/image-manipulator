e68ff29e40bf3bfea68a1731163bb6520f2898f1
tomasrobino <tomaserobino@gmail.com>
2024-03-04 23:01:09 +0100

    switched to using get-pixels to read image pixels

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const fs = require("node:fs");
-const { Buffer } = require("node:buffer");
+const getPixels = require("get-pixels");
 
-
-function main() {
-    const data = fs.readFileSync("img.jpg")
-    //buffer = Buffer.alloc(Buffer.byteLength(data));
-    //data.copy(buffer);
-    console.log(data.toString("hex"))
-}
-
-main();
+const pixelArray = [];
+getPixels("img2.jpg", (error, pixels) => {
+    if (error) {
+        console.log("Image not found");
+        return
+    }
+    //pixels.shape = [width, height, channels]
+    for (let i = 0; i < pixels.data.length; i+=pixels.shape[2]) {
+        let aux = [];
+        for (let t = 0; t < pixels.shape[2]; t++) {
+            aux.push(pixels.data[i+t]);
+        }
+        pixelArray.push(aux);
+    }
+    console.log(pixelArray)
+})
